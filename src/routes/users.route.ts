3d7a4c2e449ffee05675f19db8259b770e9b81a1
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-02-21 10:10:08 -0300

    Criando a segunda rota com parametro tipificado

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -11,6 +11,12 @@ const usersRoute = Router();
 usersRoute.get('/users', (request: Request, response: Response, next: NextFunction) => {
   const users = [{userName: 'Cristiano'}];
   response.status(200).send(users);
-})
+});
+
+usersRoute.get('/users/:uuid', (request: Request<{uuid: string}>, response: Response, next: NextFunction) => {
+  const uuid = request.params.uuid;
+
+  response.status(200).send(uuid);
+});
 
 export default usersRoute;
