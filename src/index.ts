214c8f3c40fc043a31df0ed4d1915cecd6372f20
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-02-21 09:08:43 -0300

    Criando a primeira rota get no express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello word!');
+import express , { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', ( request: Request, response: Response, next: NextFunction ) => {
+  response.status(200).send({'Hello':'world'});
+});
+
+app.listen(3000, () => {
+  console.log('Node Server Port 3000');
+});
