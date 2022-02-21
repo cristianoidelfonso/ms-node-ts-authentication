b5a8c59f64579a3b478abeea928f8c1e50666466
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-02-21 10:19:33 -0300

    Instalando e utilizando a lib http-status-codes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -5,18 +5,18 @@
 // delete /users/:uuid
 
 import { Router, Request, Response, NextFunction } from 'express';
+import { StatusCodes} from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (request: Request, response: Response, next: NextFunction) => {
   const users = [{userName: 'Cristiano'}];
-  response.status(200).send(users);
+  response.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get('/users/:uuid', (request: Request<{uuid: string}>, response: Response, next: NextFunction) => {
   const uuid = request.params.uuid;
-
-  response.status(200).send(uuid);
+  response.status(StatusCodes.OK).send({uuid});
 });
 
 export default usersRoute;
